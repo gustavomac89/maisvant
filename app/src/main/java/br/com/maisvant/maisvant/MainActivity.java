bde76b1131ab1cc324413ee32a9fd1f43549b7b3
gustavomac89 <gustavomac89@gmail.com>
2018-03-23 20:47:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/maisvant/maisvant/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.maisvant.maisvant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mais = (Button) findViewById(R.id.botaomais);
+        i = new Intent(this, ListaActivity.class);
+        mais.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startActivity(i);
+            }
+        });
+    }
+}
