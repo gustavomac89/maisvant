8ffa5448f31a5d1d550e52b80df230928f2045c8
gustavomac89 <gustavomac89@gmail.com>
2018-04-04 11:36:25 -0300

    Projeto do Vant: *Populado alguns itens para teste *Adcionado um botão com seleção para adicionar item

--- app/src/main/java/br/com/maisvant/maisvant/MainActivity.java
+++ app/src/main/java/br/com/maisvant/maisvant/MainActivity.java
@@ -3,23 +3,41 @@ package br.com.maisvant.maisvant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent i;
+    String[] dados = new String[] { "Combustivel","Estacionamento","Manutenção","Imposto", "Outros" };
+    String[] recentes = new String[] { "10,00","20,00","30,00","800,00", "250,00" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button mais = (Button) findViewById(R.id.buttonmais);
         i = new Intent(this, ListaActivity.class);
+        final ListView listview = (ListView) findViewById(R.id.lista);
+        final ListView rec = (ListView) findViewById(R.id.recentes);
+        ArrayAdapter<String> adRecente = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recentes);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dados);
+
+        listview.setAdapter(adapter);
+        rec.setAdapter(adRecente);
         mais.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                startActivity(i);
+                if (listview.getVisibility() == View.VISIBLE) {
+                    listview.setVisibility(View.INVISIBLE);
+                }else{
+                    listview.setVisibility(View.VISIBLE);
+                }
             }
         });
+
     }
 }
